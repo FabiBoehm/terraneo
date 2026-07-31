GabrielRobl/terraneoX

Added dimensional pressure output at lower gridlevel + moved writing funtionality to src/io.hpp
A second output directory is created for output at max_level -1 , which currently only holds pressure.
Core writing functionalities as well as scaling and rescaling for
dimensionalisation are now contained in function write_xdmf() in
src/io.hpp.

apps/mantlecirculation/src/parameters.hpp
@@ -122,8 +122,9 @@ struct TimeSteppingParameters
 
 struct IOParameters
 {
-    std::string outdir    = "output";
-    bool        overwrite = false;
+    std::string outdir          = "output";
+    bool        overwrite       = false;
+    bool        output_pressure = true;
 
     std::string xdmf_dir                = "xdmf";
     std::string radial_profiles_out_dir = "radial_profiles";
@@ -391,6 +392,7 @@ inline util::Result< std::variant< CLIHelp, Parameters > > parse_parameters( int
 
     add_option_with_default( app, "--outdir", parameters.io_params.outdir )->group( "I/O" );
     add_flag_with_default( app, "--outdir-overwrite", parameters.io_params.overwrite )->group( "I/O" );
+    add_option_with_default( app, "--output-pressure", parameters.io_params.output_pressure )->group( "I/O" );
 
     add_option_with_default( app, "--checkpoint-dir", parameters.io_params.checkpoint_dir )->group( "I/O" );
     add_option_with_default( app, "--checkpoint-step", parameters.io_params.checkpoint_step )->group( "I/O" );

FabiBoehm/terraneo

apps/mantlecirculation/src/parameters.hpp
@@ -292,8 +292,9 @@ struct TimeSteppingParameters
 
 struct IOParameters
 {
-    std::string outdir    = "output";
-    bool        overwrite = false;
+    std::string outdir          = "output";
+    bool        overwrite       = false;
+    bool        output_pressure = true;
 
     std::string xdmf_dir                = "xdmf";
     std::string radial_profiles_out_dir = "radial_profiles";
@@ -762,6 +763,7 @@ inline util::Result< std::variant< CLIHelp, Parameters > > parse_parameters( int
 
     add_option_with_default( app, "--outdir", parameters.io_parameters.outdir )->group( "I/O" );
     add_flag_with_default( app, "--outdir-overwrite", parameters.io_parameters.overwrite )->group( "I/O" );
+    add_option_with_default( app, "--output-pressure", parameters.io_parameters.output_pressure )->group( "I/O" );
 
     add_option_with_default( app, "--checkpoint-dir", parameters.io_parameters.checkpoint_dir )->group( "I/O" );
     add_option_with_default( app, "--checkpoint-step", parameters.io_parameters.checkpoint_step )->group( "I/O" );