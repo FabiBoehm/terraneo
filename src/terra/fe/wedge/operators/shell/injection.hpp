98542b8775e92cff5d473b9bc69e6f7c43418846
Gabriel Robl <gabriel.robl@lmu.de>
2026-08-21 16:47:16 +0200

    Injection streamline.
    Removed Injection class for VectorData, replaced by
    vector-component-wise scalar injection.

--- src/terra/fe/wedge/operators/shell/injection.hpp
+++ src/terra/fe/wedge/operators/shell/injection.hpp
@@ -5,6 +5,7 @@
 #include "grid/shell/spherical_shell.hpp"
 #include "linalg/vector.hpp"
 #include "linalg/vector_q1.hpp"
+#include "terra/kernels/common/grid_operations.hpp"
 
 namespace terra::fe::wedge::operators::shell {
 
@@ -22,17 +23,10 @@ class Injection
     grid::Grid4DDataScalar< ScalarType > src_;
     grid::Grid4DDataScalar< ScalarType > dst_;
 
-  public:
-    Injection( const grid::shell::DistributedDomain& domain_coarse )
-    : domain_coarse_( domain_coarse )
-    {}
-
-    void apply_impl( const SrcVectorType& src, DstVectorType& dst )
+    void apply_impl_grid_data( const grid::Grid4DDataScalar< ScalarT >& src, grid::Grid4DDataScalar< ScalarT >& dst )
     {
-        assign( dst, 0 );
-
-        src_ = src.grid_data();
-        dst_ = dst.grid_data();
+        src_ = src;
+        dst_ = dst;
 
         if ( src_.extent( 0 ) != dst_.extent( 0 ) )
         {
@@ -61,6 +55,25 @@ class Injection
             *this );
 
         Kokkos::fence();
+    }
+
+  public:
+    Injection( const grid::shell::DistributedDomain& domain_coarse )
+    : domain_coarse_( domain_coarse )
+    {}
+
+    void apply_impl( const SrcVectorType& src, DstVectorType& dst )
+    {
+        assign( dst, 0 );
+
+        apply_impl_grid_data( src.grid_data(), dst.grid_data() );
+    }
+
+    void apply_impl( const grid::Grid4DDataScalar< ScalarT >& src, grid::Grid4DDataScalar< ScalarT >& dst )
+    {
+        kernels::common::set_constant( dst, 0.0 );
+
+        apply_impl_grid_data( src, dst );
     }
 
     KOKKOS_INLINE_FUNCTION void
@@ -74,91 +87,30 @@ class Injection
     }
 };
 
-template < typename ScalarT, int VecDim = 3 >
-class InjectionVec
-{
-  public:
-    using SrcVectorType = linalg::VectorQ1Vec< ScalarT, VecDim >;
-    using DstVectorType = linalg::VectorQ1Vec< ScalarT, VecDim >;
-    using ScalarType    = ScalarT;
-
-  private:
-    grid::shell::DistributedDomain domain_coarse_;
-
-    grid::Grid4DDataVec< ScalarType, VecDim > src_;
-    grid::Grid4DDataVec< ScalarType, VecDim > dst_;
-
-  public:
-    InjectionVec( const grid::shell::DistributedDomain& domain_coarse )
-    : domain_coarse_( domain_coarse )
-    {}
-
-    void apply_impl( const SrcVectorType& src, DstVectorType& dst )
-    {
-        assign( dst, 0 );
-
-        src_ = src.grid_data();
-        dst_ = dst.grid_data();
-
-        if ( src_.extent( 0 ) != dst_.extent( 0 ) )
-        {
-            throw std::runtime_error( "Injection: src and dst must have the same number of subdomains." );
-        }
-
-        for ( int i = 1; i <= 3; i++ )
-        {
-            if ( src_.extent( i ) - 1 != 2 * ( dst_.extent( i ) - 1 ) )
-            {
-                throw std::runtime_error( "Injection: src and dst must have a compatible number of cells." );
-            }
-        }
-
-        // Looping over the coarse grid.
-        Kokkos::parallel_for(
-            "matvec",
-            Kokkos::MDRangePolicy< Kokkos::Rank< 4 > >(
-                { 0, 0, 0, 0 },
-                {
-                    dst_.extent( 0 ),
-                    dst_.extent( 1 ),
-                    dst_.extent( 2 ),
-                    dst_.extent( 3 ),
-                } ),
-            *this );
-
-        Kokkos::fence();
-    }
-
-    KOKKOS_INLINE_FUNCTION void
-        operator()( const int local_subdomain_id, const int x_coarse, const int y_coarse, const int r_coarse ) const
-    {
-        const auto x_fine = 2 * x_coarse;
-        const auto y_fine = 2 * y_coarse;
-        const auto r_fine = 2 * r_coarse;
-
-        for ( int d = 0; d < VecDim; ++d )
-        {
-            dst_( local_subdomain_id, x_coarse, y_coarse, r_coarse, d ) =
-                src_( local_subdomain_id, x_fine, y_fine, r_fine, d );
-        }
-    }
-};
+// Vector injection is implemented by applying the scalar Injection operator once per component.
+// See inject() overload below.
 
 template < typename ScalarT >
 inline void inject(
-    Injection< ScalarT >                     inj,
+    const grid::shell::DistributedDomain&    domain_coarse,
     const linalg::VectorQ1Scalar< ScalarT >& src,
     linalg::VectorQ1Scalar< ScalarT >&       dst )
 {
+    Injection< ScalarT > inj( domain_coarse );
     inj.apply_impl( src, dst );
 }
 
-template < typename ScalarT, int VecDim >
+template < typename ScalarT, int VecDim = 3 >
 inline void inject(
-    InjectionVec< ScalarT, VecDim >               inj,
+    const grid::shell::DistributedDomain&         domain_coarse,
     const linalg::VectorQ1Vec< ScalarT, VecDim >& src,
     linalg::VectorQ1Vec< ScalarT, VecDim >&       dst )
 {
-    inj.apply_impl( src, dst );
+    Injection< ScalarT > inj( domain_coarse );
+
+    for ( int d = 0; d < VecDim; ++d )
+    {
+        inj.apply_impl( src.grid_data().comp_[d], dst.grid_data().comp_[d] );
+    }
 }
 } // namespace terra::fe::wedge::operators::shell
