GabrielRobl/terraneoX

Small clarity update

apps/mantlecirculation/mantlecirculation.cpp
@@ -865,14 +865,14 @@ Result<> run( const Parameters& prm )
     if ( prm.devel_params.output_dimensional )
     {
         // Redimensionalise ...
-        scale( T.grid_data(), prm.boundary_params.delta_T_dimensional );
+        scale( T.grid_data(), prm.boundary_params.delta_T_K );
         scale( u.block_1().grid_data(), prm.physics_params.calc_cm_per_year );
         scale( eta[velocity_level].grid_data(), prm.physics_params.viscosity_params.reference_viscosity );
 
         xdmf_output.write();
 
         // ... and nondimensionalise again
-        scale( T.grid_data(), 1.0 / prm.boundary_params.delta_T_dimensional );
+        scale( T.grid_data(), 1.0 / prm.boundary_params.delta_T_K );
         scale( u.block_1().grid_data(), 1.0 / prm.physics_params.calc_cm_per_year );
         scale( eta[velocity_level].grid_data(), 1.0 / prm.physics_params.viscosity_params.reference_viscosity );
     }
@@ -1053,14 +1053,14 @@ Result<> run( const Parameters& prm )
         if ( prm.devel_params.output_dimensional )
         {
             // Redimensionalise ...
-            scale( T.grid_data(), prm.boundary_params.delta_T_dimensional );
+            scale( T.grid_data(), prm.boundary_params.delta_T_K );
             scale( u.block_1().grid_data(), prm.physics_params.calc_cm_per_year );
             scale( eta[velocity_level].grid_data(), prm.physics_params.viscosity_params.reference_viscosity );
 
             xdmf_output.write();
 
             // ... and nondimensionalise again
-            scale( T.grid_data(), 1.0 / prm.boundary_params.delta_T_dimensional );
+            scale( T.grid_data(), 1.0 / prm.boundary_params.delta_T_K );
             scale( u.block_1().grid_data(), 1.0 / prm.physics_params.calc_cm_per_year );
             scale( eta[velocity_level].grid_data(), 1.0 / prm.physics_params.viscosity_params.reference_viscosity );
         }

apps/mantlecirculation/src/parameters.hpp
@@ -15,11 +15,13 @@ struct MeshParameters
     int refinement_level_mesh_max   = 4;
     int refinement_level_subdomains = 0;
 
-    double radius_min       = 0.5;
-    double radius_max       = 1.0;
-    double radius_surface   = 6371000.0;
-    double radius_cmb       = 3480000.0;
-    double mantle_thickness = 2891000.0;
+    // Nondimensional radii
+    double radius_min = 0.5;
+    double radius_max = 1.0;
+    // Dimensional radii in meter
+    double radius_surface_m   = 6371000.0;
+    double radius_cmb_m       = 3480000.0;
+    double mantle_thickness_m = 2891000.0;
 };
 
 struct PlateParameters
@@ -40,11 +42,13 @@ struct BoundaryConditionsParameters
 
     double plate_velocity_scaling = 1.0;
 
-    double temperature_cmb     = 3800.0;
-    double temperature_surface = 300.0;
-    double delta_T_dimensional = temperature_cmb - temperature_surface;
-    double temperature_min     = 0.0;
-    double temperature_max     = 1.0;
+    // Nondimensional temperatures
+    double temperature_min = 0.0;
+    double temperature_max = 1.0;
+    // Dimensional temperatures in Kelvin
+    double temperature_cmb_K     = 3800.0;
+    double temperature_surface_K = 300.0;
+    double delta_T_K             = temperature_cmb_K - temperature_surface_K;
 
     PlateParameters plate_params{};
 };
@@ -63,12 +67,14 @@ struct PhysicsParameters
 {
     double gravity = 9.81;
 
+    // Non-dimensional numbers
+    double rayleigh_number    = 1e5;
+    double peclet_number      = 1.0;
+    double dissipation_number = 1.0;
+    double h_number           = 1.0;
+
     double thermal_diffusivity     = 1.0;
-    double rayleigh_number         = 1e5;
-    double peclet_number           = 1.0;
-    double dissipation_number      = 1.0;
-    double h_number                = 1.0;
-    double characteristic_velocity = 1e-10;
+    double characteristic_velocity = 1e-10; // characteristic diffusive velocity
 
     double reference_density      = 4500;
     double thermal_expansivity    = 2.5e-5;
@@ -161,17 +167,17 @@ inline void nondimensionalise( Parameters& prm )
     // --- Domain ---
 
     // radius_max is unchanged from default, always 1.0 per construction
-    mesh.radius_min       = mesh.radius_cmb / mesh.radius_surface;
-    mesh.mantle_thickness = mesh.radius_surface - mesh.radius_cmb;
+    mesh.radius_min         = mesh.radius_cmb_m / mesh.radius_surface_m;
+    mesh.mantle_thickness_m = mesh.radius_surface_m - mesh.radius_cmb_m;
 
     // --- Boundary conditions ---
 
-    boundary.temperature_min = boundary.temperature_surface / boundary.delta_T_dimensional;
-    boundary.temperature_max = boundary.temperature_cmb / boundary.delta_T_dimensional;
+    boundary.temperature_min = boundary.temperature_surface_K / boundary.delta_T_K;
+    boundary.temperature_max = boundary.temperature_cmb_K / boundary.delta_T_K;
 
     // Compute characteristic velocity and thermal diffusivity
     phys.characteristic_velocity =
-        phys.thermal_conductivity / ( phys.reference_density * phys.specific_heat_capacity * mesh.mantle_thickness );
+        phys.thermal_conductivity / ( phys.reference_density * phys.specific_heat_capacity * mesh.mantle_thickness_m );
 
     phys.thermal_diffusivity = phys.thermal_conductivity / ( phys.reference_density * phys.specific_heat_capacity );
 
@@ -183,19 +189,19 @@ inline void nondimensionalise( Parameters& prm )
         // Compute nondimensional numbers
         // Rayleigh number = ( rho * alpha * g * L^3 * dT ) / ( eta * kappa )
         phys.rayleigh_number = ( phys.reference_density * phys.gravity * phys.thermal_expansivity *
-                                 std::pow( mesh.mantle_thickness, 3 ) * boundary.delta_T_dimensional ) /
+                                 std::pow( mesh.mantle_thickness_m, 3 ) * boundary.delta_T_K ) /
                                ( phys.viscosity_params.reference_viscosity * phys.thermal_diffusivity );
 
         // Peclet number = ( U * L ) / kappa -> should be 1
-        phys.peclet_number = ( phys.characteristic_velocity * mesh.mantle_thickness ) / phys.thermal_diffusivity;
+        phys.peclet_number = ( phys.characteristic_velocity * mesh.mantle_thickness_m ) / phys.thermal_diffusivity;
 
         // Dissipation number = ( alpha * g * L ) / Cp
         phys.dissipation_number =
-            ( phys.thermal_expansivity * phys.gravity * mesh.mantle_thickness ) / phys.specific_heat_capacity;
+            ( phys.thermal_expansivity * phys.gravity * mesh.mantle_thickness_m ) / phys.specific_heat_capacity;
 
         // H-number = ( H * L ) / ( Cp * U * dT )
-        phys.h_number = ( phys.internal_heating_rate * mesh.mantle_thickness ) /
-                        ( phys.specific_heat_capacity * phys.characteristic_velocity * boundary.delta_T_dimensional );
+        phys.h_number = ( phys.internal_heating_rate * mesh.mantle_thickness_m ) /
+                        ( phys.specific_heat_capacity * phys.characteristic_velocity * boundary.delta_T_K );
     }
 }
 
@@ -235,8 +241,8 @@ inline util::Result< std::variant< CLIHelp, Parameters > > parse_parameters( int
     add_option_with_default( app, "--refinement-level-subdomains", parameters.mesh_params.refinement_level_subdomains )
         ->group( "Domain" );
 
-    add_option_with_default( app, "--radius-cmb", parameters.mesh_params.radius_cmb )->group( "Domain" );
-    add_option_with_default( app, "--radius-surface", parameters.mesh_params.radius_surface )->group( "Domain" );
+    add_option_with_default( app, "--radius-cmb", parameters.mesh_params.radius_cmb_m )->group( "Domain" );
+    add_option_with_default( app, "--radius-surface", parameters.mesh_params.radius_surface_m )->group( "Domain" );
 
     ///////////////////////////
     /// Boundary conditions ///
@@ -262,10 +268,10 @@ inline util::Result< std::variant< CLIHelp, Parameters > > parse_parameters( int
         ->default_val( "noslip" )
         ->group( "Boundary Conditions" );
 
-    add_option_with_default( app, "--temperature-cmb", parameters.boundary_params.temperature_cmb )
+    add_option_with_default( app, "--temperature-cmb", parameters.boundary_params.temperature_cmb_K )
         ->group( "Boundary Conditions" );
 
-    add_option_with_default( app, "--temperature-surface", parameters.boundary_params.temperature_surface )
+    add_option_with_default( app, "--temperature-surface", parameters.boundary_params.temperature_surface_K )
         ->group( "Boundary Conditions" );
 
     //////////////////////////////

FabiBoehm/terraneo

apps/mantlecirculation/mantlecirculation.cpp
@@ -313,14 +313,14 @@ Result<> run( const Parameters& prm )
     if ( prm.devel_parameters.output_dimensional )
     {
         // Redimensionalise ...
-        scale( T.grid_data(), prm.boundary_parameters.delta_T_dimensional );
+        scale( T.grid_data(), prm.boundary_parameters.delta_T_K );
         scale( u.block_1().grid_data(), prm.physics_parameters.calc_cm_per_year );
         scale( eta[velocity_level].grid_data(), prm.physics_parameters.viscosity_parameters.reference_viscosity );
 
         xdmf_output.write();
 
         // ... and nondimensionalise again
-        scale( T.grid_data(), 1.0 / prm.boundary_parameters.delta_T_dimensional );
+        scale( T.grid_data(), 1.0 / prm.boundary_parameters.delta_T_K );
         scale( u.block_1().grid_data(), 1.0 / prm.physics_parameters.calc_cm_per_year );
         scale( eta[velocity_level].grid_data(), 1.0 / prm.physics_parameters.viscosity_parameters.reference_viscosity );
     }
@@ -525,7 +525,7 @@ Result<> run( const Parameters& prm )
             if ( prm.devel_parameters.output_dimensional )
             {
                 // Redimensionalise ...
-                scale( T.grid_data(), prm.boundary_parameters.delta_T_dimensional );
+                scale( T.grid_data(), prm.boundary_parameters.delta_T_K );
                 scale( u.block_1().grid_data(), prm.physics_parameters.calc_cm_per_year );
                 scale(
                     eta[velocity_level].grid_data(), prm.physics_parameters.viscosity_parameters.reference_viscosity );
@@ -533,7 +533,7 @@ Result<> run( const Parameters& prm )
                 xdmf_output.write();
 
                 // ... and nondimensionalise again
-                scale( T.grid_data(), 1.0 / prm.boundary_parameters.delta_T_dimensional );
+                scale( T.grid_data(), 1.0 / prm.boundary_parameters.delta_T_K );
                 scale( u.block_1().grid_data(), 1.0 / prm.physics_parameters.calc_cm_per_year );
                 scale(
                     eta[velocity_level].grid_data(),

apps/mantlecirculation/src/parameters.hpp
@@ -17,11 +17,13 @@ struct MeshParameters
     int refinement_level_mesh_max   = 4;
     int refinement_level_subdomains = 0;
 
-    double radius_min       = 0.5;
-    double radius_max       = 1.0;
-    double radius_surface   = 6371000.0;
-    double radius_cmb       = 3480000.0;
-    double mantle_thickness = 2891000.0;
+    // Nondimensional radii
+    double radius_min = 0.5;
+    double radius_max = 1.0;
+    // Dimensional radii in meter
+    double radius_surface_m   = 6371000.0;
+    double radius_cmb_m       = 3480000.0;
+    double mantle_thickness_m = 2891000.0;
 
     // Anisotropic refinement (Path B.1): radial diamond level is set to
     // (refinement_level_mesh_* + radial_extra_levels) at every MG level, so the
@@ -65,11 +67,13 @@ struct BoundaryConditionsParameters
 
     double plate_velocity_scaling = 1.0;
 
-    double temperature_cmb     = 3800.0;
-    double temperature_surface = 300.0;
-    double delta_T_dimensional = temperature_cmb - temperature_surface;
-    double temperature_min     = 0.0;
-    double temperature_max     = 1.0;
+    // Nondimensional temperatures
+    double temperature_min = 0.0;
+    double temperature_max = 1.0;
+    // Dimensional temperatures in Kelvin
+    double temperature_cmb_K     = 3800.0;
+    double temperature_surface_K = 300.0;
+    double delta_T_K             = temperature_cmb_K - temperature_surface_K;
 
     PlateParameters plate_parameters{};
 };
@@ -160,12 +164,14 @@ struct PhysicsParameters
 {
     double gravity = 9.81;
 
+    // Non-dimensional numbers
+    double rayleigh_number    = 1e5;
+    double peclet_number      = 1.0;
+    double dissipation_number = 1.0;
+    double h_number           = 1.0;
+
     double thermal_diffusivity     = 1.0;
-    double rayleigh_number         = 1e5;
-    double peclet_number           = 1.0;
-    double dissipation_number      = 1.0;
-    double h_number                = 1.0;
-    double characteristic_velocity = 1e-10;
+    double characteristic_velocity = 1e-10; // characteristic diffusive velocity
 
     double reference_density      = 4500;
     double thermal_expansivity    = 2.5e-5;
@@ -337,17 +343,17 @@ inline void nondimensionalise( Parameters& prm )
     // --- Domain ---
 
     // radius_max is unchanged from default, always 1.0 per construction
-    mesh.radius_min       = mesh.radius_cmb / mesh.radius_surface;
-    mesh.mantle_thickness = mesh.radius_surface - mesh.radius_cmb;
+    mesh.radius_min         = mesh.radius_cmb_m / mesh.radius_surface_m;
+    mesh.mantle_thickness_m = mesh.radius_surface_m - mesh.radius_cmb_m;
 
     // --- Boundary conditions ---
 
-    boundary.temperature_min = boundary.temperature_surface / boundary.delta_T_dimensional;
-    boundary.temperature_max = boundary.temperature_cmb / boundary.delta_T_dimensional;
+    boundary.temperature_min = boundary.temperature_surface_K / boundary.delta_T_K;
+    boundary.temperature_max = boundary.temperature_cmb_K / boundary.delta_T_K;
 
     // Compute characteristic velocity and thermal diffusivity
     phys.characteristic_velocity =
-        phys.thermal_conductivity / ( phys.reference_density * phys.specific_heat_capacity * mesh.mantle_thickness );
+        phys.thermal_conductivity / ( phys.reference_density * phys.specific_heat_capacity * mesh.mantle_thickness_m );
 
     phys.thermal_diffusivity = phys.thermal_conductivity / ( phys.reference_density * phys.specific_heat_capacity );
 
@@ -359,19 +365,19 @@ inline void nondimensionalise( Parameters& prm )
         // Compute nondimensional numbers
         // Rayleigh number = ( rho * alpha * g * L^3 * dT ) / ( eta * kappa )
         phys.rayleigh_number = ( phys.reference_density * phys.gravity * phys.thermal_expansivity *
-                                 std::pow( mesh.mantle_thickness, 3 ) * boundary.delta_T_dimensional ) /
+                                 std::pow( mesh.mantle_thickness_m, 3 ) * boundary.delta_T_K ) /
                                ( phys.viscosity_parameters.reference_viscosity * phys.thermal_diffusivity );
 
         // Peclet number = ( U * L ) / kappa -> should be 1
-        phys.peclet_number = ( phys.characteristic_velocity * mesh.mantle_thickness ) / phys.thermal_diffusivity;
+        phys.peclet_number = ( phys.characteristic_velocity * mesh.mantle_thickness_m ) / phys.thermal_diffusivity;
 
         // Dissipation number = ( alpha * g * L ) / Cp
         phys.dissipation_number =
-            ( phys.thermal_expansivity * phys.gravity * mesh.mantle_thickness ) / phys.specific_heat_capacity;
+            ( phys.thermal_expansivity * phys.gravity * mesh.mantle_thickness_m ) / phys.specific_heat_capacity;
 
         // H-number = ( H * L ) / ( Cp * U * dT )
-        phys.h_number = ( phys.internal_heating_rate * mesh.mantle_thickness ) /
-                        ( phys.specific_heat_capacity * phys.characteristic_velocity * boundary.delta_T_dimensional );
+        phys.h_number = ( phys.internal_heating_rate * mesh.mantle_thickness_m ) /
+                        ( phys.specific_heat_capacity * phys.characteristic_velocity * boundary.delta_T_K );
     }
 }
 
@@ -439,8 +445,8 @@ inline util::Result< std::variant< CLIHelp, Parameters > > parse_parameters( int
         app, "--refinement-level-subdomains", parameters.mesh_parameters.refinement_level_subdomains )
         ->group( "Domain" );
 
-    add_option_with_default( app, "--radius-min", parameters.mesh_parameters.radius_min )->group( "Domain" );
-    add_option_with_default( app, "--radius-max", parameters.mesh_parameters.radius_max )->group( "Domain" );
+    add_option_with_default( app, "--radius-cmb", parameters.mesh_parameters.radius_cmb_m )->group( "Domain" );
+    add_option_with_default( app, "--radius-surface", parameters.mesh_parameters.radius_surface_m )->group( "Domain" );
 
     add_option_with_default( app, "--radial-extra-levels", parameters.mesh_parameters.radial_extra_levels )
         ->group( "Domain" )
@@ -498,10 +504,10 @@ inline util::Result< std::variant< CLIHelp, Parameters > > parse_parameters( int
         ->default_val( "noslip" )
         ->group( "Boundary Conditions" );
 
-    add_option_with_default( app, "--temperature-cmb", parameters.boundary_parameters.temperature_cmb )
+    add_option_with_default( app, "--temperature-cmb", parameters.boundary_parameters.temperature_cmb_K )
         ->group( "Boundary Conditions" );
 
-    add_option_with_default( app, "--temperature-surface", parameters.boundary_parameters.temperature_surface )
+    add_option_with_default( app, "--temperature-surface", parameters.boundary_parameters.temperature_surface_K )
         ->group( "Boundary Conditions" );
 
     //////////////////////////////